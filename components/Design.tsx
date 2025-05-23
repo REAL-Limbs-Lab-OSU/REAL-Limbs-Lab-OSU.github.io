098b9542c3e0cc5946f516f929c704cf8632a30f
rposh <74609418+rposh@users.noreply.github.com>
2025-05-22 21:02:30 -0400

    Update Design.tsx

--- components/Design.tsx
+++ components/Design.tsx
@@ -5,24 +5,24 @@ import { loadFull } from "tsparticles";
 import BannerImg from "../public/assets/bg.jpg";
 
 const Design = () => {
-  const particlesInit = useCallback(async (engine: Engine) => {
+  // const particlesInit = useCallback(async (engine: Engine) => {
     // console.log(engine);
 
     // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
+    // await loadFull(engine);
+  // }, []);
 
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {
+  // const particlesLoaded = useCallback(
+    // async (container: Container | undefined) => {
       // await console.log(container);
-    },
-    []
-  );
+    // },
+    // []
+  // );
   return (
     <div className="w-full h-full bg-hero-image bg-cover relative">
-      <Particles
+{/*       <Particles
         className="w-full h-full"
         id="tsparticles"
         init={particlesInit}
@@ -95,7 +95,7 @@ const Design = () => {
           },
           detectRetina: true,
         }}
-      />
+      /> */}
     </div>
   );
 };
