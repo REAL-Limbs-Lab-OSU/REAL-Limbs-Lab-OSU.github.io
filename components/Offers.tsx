b1f635dd8e9d864c453db00f0edc6a356e6a93f6
rposh <74609418+rposh@users.noreply.github.com>
2025-05-22 23:13:09 -0400

    Update Offers.tsx

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { RxCopy } from "react-icons/rx";
+import { FaPersonWalking } from "react-icons/fa6";
+import { GiGears } from "react-icons/gi";
+import { LuBrainCircuit } from "react-icons/lu";
 import { SiAntdesign } from "react-icons/si";
 import { FaChartPie, FaConnectdevelop } from "react-icons/fa";
 import OfferCard from "./OfferCard";
@@ -16,25 +19,25 @@ const Offers = () => {
             title="Biomimetic Control"
             subTitle="We develop control systems that emulate natural human locomotion, enabling prosthetic 
             limbs that are intuitive, adaptive, reliable, and highly functional."
-            Icon={RxCopy}
+            Icon={FaConnectdevelop}
           />
           <OfferCard
             title="Prosthesis Design"
             subTitle="We engineer advanced prostheses that seek to integrate seamlessly with 
             the human body, enhancing functionality, mobility, and comfort through innovative design."
-            Icon={SiAntdesign}
+            Icon={GiGears}
           />
           <OfferCard
             title="Biomechanics Studies"
             subTitle="We analyze human movement patterns to inform the development of prosthetic 
             technologies that restore and augment user mobility."
-            Icon={FaChartPie}
+            Icon={FaPersonWalking}
           />
           <OfferCard
             title="Human-Robot Interaction"
             subTitle="We design intuitive interfaces and study user–prosthesis interaction to ensure usefulness, 
             promote seamless integration, and enhance embodiment."
-            Icon={FaConnectdevelop}
+            Icon={LuBrainCircuit}
           />
         </div>
       </div>
