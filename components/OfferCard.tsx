7829281ac0953010d54441f19218e8ad72115da1
rposh <74609418+rposh@users.noreply.github.com>
2025-05-22 22:54:33 -0400

    Update OfferCard.tsx

--- components/OfferCard.tsx
+++ components/OfferCard.tsx
@@ -10,7 +10,7 @@ const OfferCard: FunctionComponent<Props> = ({Icon,title,subTitle}) => {
   return (
     <div className="w-full shadow-cardShadow flex flex-col items-center justify-center gap-10 py-16">
       {Icon && <Icon className="text-designColor text-7xl" />}
-      <h1 className="text-2xl font-semibold text-gray-100 uppercase tracking-[10px]">
+      <h1 className="text-2xl font-semibold text-gray-100 uppercase text-center tracking-[10px]">
         {title}
       </h1>
       <p className="text-2xl text-textColor px-10 text-center leading-[40px]">
