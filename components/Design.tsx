d7a017de06d199e6f4b85ab939665b8d158c2e49
rposh <74609418+rposh@users.noreply.github.com>
2025-05-22 21:09:17 -0400

    Update Design.tsx

--- components/Design.tsx
+++ components/Design.tsx
@@ -5,99 +5,10 @@ import { loadFull } from "tsparticles";
 import BannerImg from "../public/assets/bg.jpg";
 
 const Design = () => {
-  // const particlesInit = useCallback(async (engine: Engine) => {
-    // console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    // await loadFull(engine);
-  // }, []);
-
-  // const particlesLoaded = useCallback(
-    // async (container: Container | undefined) => {
-      // await console.log(container);
-    // },
-    // []
-  // );
-  return (
-    <div className="w-full h-full bg-hero-image bg-cover relative">
-{/*       <Particles
-        className="w-full h-full"
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 60,
-          fullScreen: false,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 120,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 2,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 1000,
-              },
-              value: 90,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      /> */}
-    </div>
-  );
+  
+  return <div className="w-full h-full bg-hero-image bg-cover relative">
+  
+  </div>
 };
 
 export default Design;
